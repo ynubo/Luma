18af5f83a900883fec249465f4cf6c01ab23dcff
sacerdosxd <heartbreakexe00001@gmail.com>
2026-06-30 15:25:51 +0300

    Clean project: leave only basic main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
 use teloxide::prelude::*;
 
-// Декларируем наши модули из соседних файлов
-mod admin;
-mod commands; 
-
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    dotenvy::dotenv().ok(); 
-    println!("Бот запущен с разделением по модулям...");
-
+    dotenvy::dotenv().ok();
+    println!("Бот готов к работе!");
     let bot = Bot::from_env();
-
-    // Вызываем repl, но теперь указываем путь через имя модуля: commands::...
-    commands::Command::repl(bot, commands::answer).await;
 }
